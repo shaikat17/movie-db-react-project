323d7c4c0dd91181a69febf98987e2d5f44ecf2c
shaikat17 <shaikatpal56@gmail.com>
2023-03-24 14:11:59 +0600

    setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import SingleMovie from './components/SingleMovie'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <h2>Movie DB Project</h2>
-  )
+  return <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/movies/:id' element={<SingleMovie/>} />
+  </Routes>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./Context";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AppProvider>
   </React.StrictMode>
 );
