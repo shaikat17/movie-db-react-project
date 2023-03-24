257c5ab6a30feaf463e1ab4b80111a431bdc2369
shaikat17 <shaikatpal56@gmail.com>
2023-03-24 13:35:11 +0600

    added env file in the project

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 
 // api address
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_APP_MOVIE_DB_API_KEY
+}`;
+
+console.log(API_ENDPOINT);
 
 const AppContext = React.createContext()
 
