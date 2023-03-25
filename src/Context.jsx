8f8253edcc7605ac13db5a4cc3e85ff8152509ac
shaikat17 <shaikatpal56@gmail.com>
2023-03-26 00:01:38 +0600

    moved usefetch function to a single file

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-
+import UseFetch from "./components/UseFetch";
 // api address
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
   import.meta.env.VITE_APP_MOVIE_DB_API_KEY
@@ -11,34 +11,11 @@ const AppContext = React.createContext()
 
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState({show: false, msg: ''})
-  const [movies, setMovies] = useState([])
-  const [query, setQuery] = useState('sup')
+  const [query, setQuery] = useState("batman");
+  const { isLoading, error, data: movies } = UseFetch(`&s=${query}`);
 
-  // data fetching function 
-  const fetchMovies = async (url) => {
-    setIsLoading(true)
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      
-      if(data.Response === 'True') {
-        setMovies(data.Search)
-        setError({show: false, msg: ''})
-      } else {
-        setError({show: true, msg: data.Error})
-      }
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error)
-    }
-    // console.log(movies)
-  }
+  // console.log(movies)
 
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
-  },[query])
   return <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>{children}</AppContext.Provider>
 }
 
---
+++ src/components/UseFetch.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_APP_MOVIE_DB_API_KEY
+}`;
+
+const UseFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [data, setData] = useState(null);
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.Response === "True") {
+        setData(data.Search || data);
+
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}${urlParams}`);
+  }, [urlParams]);
+  return { isLoading, error, data };
+};
+
+export default UseFetch;
