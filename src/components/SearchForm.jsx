7ae10dcda51a90c6b82101fdb9f998ef48e9e788
shaikat17 <shaikatpal56@gmail.com>
2023-03-25 22:41:02 +0600

    working on search query form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useGlobalContext } from '../Context'
 
 const SearchForm = () => {
+const { query, setQuery, error } = useGlobalContext()
   return (
-    <div>SearchForm</div>
+    <form onSubmit={(e) => e.preventDefault()} className="search-form">
+      <h2>Search Movie</h2>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} value={query} className="form-input" />
+    </form>
   )
 }
 
